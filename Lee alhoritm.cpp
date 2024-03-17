3659bfc2328420c1f47c1f2b5c11976ced91cc2e
Kaizer1917 <155324357+Kaizer1917@users.noreply.github.com>
2024-03-17 22:38:42 +0300

    Update Lee alhoritm.cpp

--- Lee alhoritm.cpp
+++ Lee alhoritm.cpp
@@ -5,35 +5,35 @@
 
 int main(int argc, const char * argv[])
 {
-    int dl[] = {-1, 0, 1, 0}; // these arrays will help you travel in the 4 directions more easily
+    int dl[] = {-1, 0, 1, 0}; 
     int dc[] = {0, 1, 0, -1};
 
-    queue<int> X, Y; // the queues used to get the positions in the matrix
+    queue<int> X, Y; 
 
-    X.push(start_x); //initialize the queues with the start position
+    X.push(start_x); 
     Y.push(start_y);
 
     void lee()
     {
       int x, y, xx, yy;
-      while(!X.empty()) // while there are still positions in the queue
+      while(!X.empty()) 
       {
-        x = X.front(); // set the current position
+        x = X.front(); 
         y = Y.front();
         for(int i = 0; i < 4; i++)
         {
-          xx = x + dl[i]; // travel in an adiacent cell from the current position
+          xx = x + dl[i]; 
           yy = y + dc[i];
-          if('position is valid') //here you should insert whatever conditions should apply for your position (xx, yy)
+          if('position is valid') 
           {
-              X.push(xx); // add the position to the queue
+              X.push(xx); 
               Y.push(yy);
-              mat[xx][yy] = -1; // you usually mark that you have been to this position in the matrix
+              mat[xx][yy] = -1; 
           }
           
         }
         
-        X.pop(); // eliminate the first position, as you have no more use for it
+        X.pop(); 
         Y.pop();
         
       }
